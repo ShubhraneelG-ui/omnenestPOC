290be14d77603f62d4ec4cfeac2924b465151450
Amay1211 <amay.sandanshiv@omnenest.com>
2024-09-20 14:46:07 +0530

    feat: add script

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './services/temp/App.tsx';
+import App from './services/App.tsx';
 import './styles/base.scss';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(    document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
