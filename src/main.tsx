8fba60d3be98500f0387e856590e0f246976af82
Amay1211 <amay.sandanshiv@omnenest.com>
2024-09-20 14:36:24 +0530

    feat: check husky

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './services/App.tsx';
+import App from './services/temp/App.tsx';
 import './styles/base.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
